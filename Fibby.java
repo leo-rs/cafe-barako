d23c11462f2b868e3995da208af4cb9aaf0992ba
leo-rs <90270063+leo-rs@users.noreply.github.com>
2022-04-12 10:33:12 -0500

    "Fibonacci"

---
+++ Fibby.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibby {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("This program prints all fibonacci numbers up to fibonacci(n).");
+        System.out.println("Enter a number:");
+
+        int n = Integer.parseInt(in.nextLine());
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("The fibonacci number at fibonacci(" + i+ ") is " + plainFib(i));
+        }
+    }
+
+    public static int plainFib(int n) {
+        if (n==1 || n==2)
+            return 1;
+        else
+
+            return plainFib(n-1) + plainFib(n-2);
+    }
+}
